2fd8c1701697676fa682fa4b74df54b52e2cf1b0
CodAffection <dotnetmob@gmail.com>
2024-05-29 15:21:23 +0530

    1.1 - first push

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="mx-5">
+          <div>food delivery form</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
