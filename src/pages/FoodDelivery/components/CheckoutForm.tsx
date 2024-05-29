8efa0808be50070618a709bc93afd80cc61b661d
CodAffection <dotnetmob@gmail.com>
2024-05-29 16:27:28 +0530

    6.4 - first push

---
+++ src/pages/FoodDelivery/components/CheckoutForm.tsx
@@ -0,0 +1,57 @@
+import { useFormContext, useFormState } from "react-hook-form"
+import { Select } from "../../../controls/Select"
+import { useRenderCount } from "../../../hooks/useRenderCount"
+
+const paymentOptions: SelectOptionType[] = [
+  { value: "", text: "Select" },
+  { value: "online", text: "Paid Online" },
+  { value: "COD", text: "Cash On Delivery" },
+]
+
+const deliveryInOptions: SelectOptionType[] = [
+  { value: 0, text: "Select" },
+  { value: 30, text: "Half an Hour" },
+  { value: 60, text: "1 Hour" },
+  { value: 120, text: "2 Hour" },
+  { value: 180, text: "3 Hour" },
+]
+
+const RenderCount = useRenderCount()
+
+export const CheckoutForm = () => {
+  const {
+    register,
+  } = useFormContext<CheckoutFormType>()
+
+  const { errors } = useFormState<CheckoutFormType>({
+    name: ["paymentMethod", "deliveryIn"],
+    exact:true,
+  })
+
+  return (
+    <>
+      {/* <RenderCount /> */}
+      <div className="text-start fw-bold mt-4 mb-2">Checkout Details</div>
+      <div className="row mb-2">
+        <div className="col">
+          <Select
+            label="Payment Method"
+            options={paymentOptions}
+            {...register("paymentMethod", {
+              required: "This field is required.",
+            })}
+            error={errors.paymentMethod}
+          />
+        </div>
+        <div className="col">
+          <Select
+            label="Delivery Within"
+            options={deliveryInOptions}
+            {...register("deliveryIn")}
+            error={errors.deliveryIn}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
