bde7b36e23be75462524043f625bcaf8575aeac5
CodAffection <dotnetmob@gmail.com>
2024-05-29 16:44:32 +0530

    12.1 - first push

---
+++ src/pages/FoodDelivery/components/MasterFoodDeliveryForm.tsx
@@ -0,0 +1,60 @@
+import { useFormContext, useFormState } from "react-hook-form"
+import { TextField } from "../../../controls/TextField"
+import { useRenderCount } from "../../../hooks/useRenderCount"
+
+const RenderCount = useRenderCount()
+
+export const MasterFoodDeliveryForm = () => {
+  const { register } = useFormContext<MasterFoodDeliveryFormType>()
+
+  const { errors } = useFormState<MasterFoodDeliveryFormType>({
+    name: ["orderNo", "customerName", "mobile", "email"],
+    exact: true,
+  })
+
+  return (
+    <>
+      {/* <RenderCount /> */}
+      <input type="hidden" {...register("orderId")} />
+      <input type="hidden" {...register("placedOn")} />
+      <div className="row mb-2">
+        <div className="col">
+          <TextField label="#Order No." disabled {...register("orderNo")} />
+        </div>
+        <div className="col">
+          <TextField
+            label="Mobile"
+            {...register("mobile", {
+              required: "This field is required.",
+            })}
+            error={errors.mobile}
+          />
+        </div>
+      </div>
+      <div className="row mb-2">
+        <div className="col">
+          <TextField
+            label="Customer Name"
+            {...register("customerName", {
+              required: "This field is required.",
+            })}
+            error={errors.customerName}
+          />
+        </div>
+        <div className="col">
+          <TextField
+            type="email"
+            label="Email"
+            {...register("email", {
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Incorrect email format.",
+              },
+            })}
+            error={errors.email}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
